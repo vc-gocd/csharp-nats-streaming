cc163bea8596f0feebc14f3e85cbcc49154db213
Colin Sullivan <colin.sullivan@apcera.com>
2016-08-30 11:27:10 -0600

    Update documentation after API changes.

--- interface_s_t_a_n_1_1_client_1_1_i_stan_connection.js
+++ interface_s_t_a_n_1_1_client_1_1_i_stan_connection.js
@@ -2,11 +2,8 @@ var interface_s_t_a_n_1_1_client_1_1_i_stan_connection =
 [
     [ "Close", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#abab6287cffd65073188b96e729fb3134", null ],
     [ "Publish", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#ac0a3381bda7138815303172ff03da871", null ],
-    [ "Publish", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#a95573536d453ff04dc467d129d08528e", null ],
     [ "Publish", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#a3a833bb7560ae9fe7228db31810a746c", null ],
-    [ "Publish", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#ab07978a6b849b64ebf809ce541f56185", null ],
     [ "PublishAsync", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#a109cd42ac9814257dc1f6bbceae2e1f4", null ],
-    [ "PublishAsync", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#a190287811bf9287dd9547b8997d1cbc6", null ],
     [ "Subscribe", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#aa2317cda37f33f7c572fb18c1d8735c2", null ],
     [ "Subscribe", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#ac216692da889b12c6d770924c7863d64", null ],
     [ "Subscribe", "interface_s_t_a_n_1_1_client_1_1_i_stan_connection.html#a04d9a7370dd48e8ca7f9db3c6d8a8cbc", null ],
