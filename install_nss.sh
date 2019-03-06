62457371d343d462aa5c264c5c9fc80b61490697
Colin Sullivan <colin@synadia.com>
2019-03-05 21:38:47 -0700

    Update server version
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- install_nss.sh
+++ install_nss.sh
@@ -4,8 +4,9 @@ set -e
 if [ ! "$(ls -A $HOME/nss)" ]; then
   mkdir -p $HOME/nss;
   cd $HOME/nss
-  wget https://github.com/nats-io/nats-streaming-server/releases/download/v0.2.0/nats-streaming-server-linux-amd64.zip -O nss.zip;
+  wget https://github.com/nats-io/nats-streaming-server/releases/download/v0.12.0/nats-streaming-server-v0.12.0-linux-amd64.zip -O nss.zip;
   unzip nss.zip;
+  mv $HOME/nss/nats-streaming-server-v0.12.0-linux-amd64/nats-streaming-server nats-streaming-server
 else
   echo 'Using cached directory.';
 fi
